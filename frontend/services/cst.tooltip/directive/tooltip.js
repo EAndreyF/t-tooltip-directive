383180b5bbf51045ba4430c610e56e563cdc4ee5
Andrey Evstropov <genesis.andrey@yandex.ru>
2016-02-04 20:05:23 +0300

    place icon into center

--- frontend/services/cst.tooltip/directive/tooltip.js
+++ frontend/services/cst.tooltip/directive/tooltip.js
@@ -18,22 +18,51 @@
   TooltipCtrl.$inject = ['$scope', '$element'];
 
   function TooltipCtrl($scope, $element) {
+    $element = $($element[0]);
     var $el = $($scope.element);
     var $canvas = $element.find('canvas');
+    var $tooltip = $element.find('.cst-tooltip');
 
-    $scope.$on('update', update);
+    var iconSize = [36 + 40 * 2, 36];
+
+    $scope.$on('update', _update);
 
     return {
-      getCanvasStyle: getCanvasStyle
+      getCanvasStyle: getCanvasStyle,
+      getIconsStyle: getIconsStyle
     };
-
-    function update() {
-      console.debug('updated');
-    }
 
     function getCanvasStyle() {
       return {
       }
+    }
+
+    function getIconsStyle() {
+      var center = _getElCenter($el);
+      var tooltipOffset = _getElOffset($tooltip);
+      return {
+        left: center[0] + tooltipOffset[0] + 'px',
+        top: center[1] - tooltipOffset[1] + 'px'
+      }
+    }
+
+    function _update() {
+      console.debug('updated');
+    }
+
+    function _getElCenter($el) {
+      var offset = _getElOffset($el);
+      var width = $el.outerWidth();
+      var height = $el.outerHeight();
+
+      var centerX = offset[0] + width / 2;
+      var centerY = offset[1] + height / 2;
+      return [centerX, centerY];
+    }
+
+    function _getElOffset($el) {
+      var offset = $el.offset();
+      return [offset.left, offset.top];
     }
   }
 
