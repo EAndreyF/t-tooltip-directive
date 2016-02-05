6db66c58798c746afa1b53619eb8273f4a124a12
Andrey Evstropov <genesis.andrey@yandex.ru>
2016-02-05 21:49:50 +0300

    starting canvas

--- frontend/services/cst.tooltip/tooltip/tooltip.js
+++ frontend/services/cst.tooltip/tooltip/tooltip.js
@@ -1,6 +1,11 @@
 (function () {
   'use strict';
 
+  var CANVAS_WIDTH = 150;
+  var CANVAS_HEIGHT = 250;
+  var BZ_WIDTH = 50;
+  var BZ_HEIGHT = 50;
+
   angular
     .module('cst.tooltip')
     .directive('cstTooltip', TooltipDirective);
@@ -26,18 +31,21 @@
     $element = $($element[0]);
     var $el = $(tp.element);
     var $canvas = $element.find('canvas');
+    var ctx = $canvas[0].getContext("2d");
     var $tooltip = $element.find('.cst-tooltip');
 
     var iconSize = [36 + 40 * 2, 36];
 
     $scope.$on('update', _update);
 
-    $scope._getElCenter = _getElCenter;
-
     return {
       getCanvasStyle: getCanvasStyle,
       getIconsStyle: getIconsStyle,
-      getMainStyle: getMainStyle
+      getMainStyle: getMainStyle,
+      _canvasLineDraw: _canvasLineDraw,
+      _getElCenter: _getElCenter,
+      _getElOffset: _getElOffset,
+      _getCanvasPosition: _getCanvasPosition
     };
 
     function getMainStyle() {
@@ -46,18 +54,31 @@
     }
 
     function getCanvasStyle() {
+      this._canvasLineDraw();
+      var leftTop = this._getCanvasPosition();
       return {
+        width: CANVAS_WIDTH + 'px',
+        height: CANVAS_HEIGHT + 'px',
+        left: leftTop.left + 'px',
+        top: leftTop.top + 'px'
       }
     }
 
     function getIconsStyle() {
-      console.debug('updated');
-      var center = _getElCenter($el);
-      var tooltipOffset = _getElOffset($tooltip);
+      var canvasPosition =  this._getCanvasPosition();
       return {
-        left: center[0] + tooltipOffset[0] + 'px',
-        top: center[1] - tooltipOffset[1] + 'px'
+        left: canvasPosition.left + 'px',
+        top: canvasPosition.top + BZ_HEIGHT + 'px'
       }
+    }
+
+    function _getCanvasPosition() {
+      var center = this._getElCenter($el);
+      var tooltipOffset = this._getElOffset($tooltip);
+      return {
+        left: center[0] - tooltipOffset[0] - CANVAS_WIDTH,
+        top: center[1] - tooltipOffset[1] - CANVAS_HEIGHT
+      };
     }
 
     function _update() {
@@ -65,7 +86,7 @@
     }
 
     function _getElCenter($el) {
-      var offset = _getElOffset($el);
+      var offset = this._getElOffset($el);
       var width = $el.outerWidth();
       var height = $el.outerHeight();
 
@@ -77,6 +98,18 @@
     function _getElOffset($el) {
       var offset = $el.offset();
       return [offset.left, offset.top];
+    }
+
+    function _canvasLineDraw() {
+      var width = $canvas.width();
+      var height = $canvas.height();
+      ctx.clearRect(0, 0, width, height);
+      ctx.beginPath();
+      ctx.moveTo(width, height);
+      ctx.bezierCurveTo(BZ_WIDTH, BZ_HEIGHT, BZ_WIDTH, BZ_HEIGHT, 0, BZ_HEIGHT);
+      ctx.lineWidth = 2;
+      ctx.strokeStyle = "#6eb41d";
+      ctx.stroke();
     }
   }
 
